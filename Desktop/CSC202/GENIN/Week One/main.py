ac07383ec4bd9840e9231e9b59804a08a03dca76
sokebiz <oladsodeeq148@gmail.com>
2023-06-19 23:33:01 +0100

    day one

---
+++ Desktop/CSC202/GENIN/Week One/main.py
@@ -0,0 +1,9 @@
+print("Hello world!")
+
+#writing my day one code below here
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+print("Hello world!\n Hello world!\nHello world!")
+print("Hello" + "World of SokeBiz")
+print("Hello" + " " + "World of SokeBiz")
